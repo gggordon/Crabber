49a44cabdf9d4c7597672587cff91b24b7d3026c
gggordon <ggg_gilroy@yahoo.com>
2015-11-06 02:11:15 +0200

    Added Wrappers, Processors and Formatters

--- crabber.js
+++ crabber.js
@@ -9,12 +9,98 @@
  */
 
  (function($,w){
+ 	if($==undefined || $ == null){
+ 		throw new Exception("jQuery required for Crabber");
+ 	}
+ 	/**
+     * @param props Object Configuration Object
+     * @param props.title String Title
+     * @param props.description String Page Description
+     * @param props.videoUrl String Url of Video
+     * @param props.url String Link Url
+     * @param props.videoType String ** Not Used
+     * @param props.imageUrl String ** Not Used
+ 	 */
+ 	function ExternalContent(props){
+        props = props || {};
+        var _self = this;
+        _self.title = props.title || "";
+        _self.url = props.url || "";
+        _self.description = props.description || "";
+        _self.type = props.type || "";
+        _self.imageUrl = props.imageUrl || "";
+        
+        /**
+         * Display HTML 
+         * @param options Object
+         * @overide
+         */
+        _self.display=function(options){
+            return '<div class=""></div>';
+        }
+ 	}
+
+ 	/**
+ 	 * @param divNode HTMLInputElement
+ 	 * @param options Object
+ 	 * @param options.
+ 	 */
  	function Crabber(divNode, options){
         if(divNode == null || divNode == undefined)
         	return;
+
         options = options || {};
         var _self = this;
+        
         _self._$tbox=$(divNode);
+        _self._$viewNode = $(options.viewNode || (function($tbox){
+            var uuid= "crab-view-"+Math.random()*50;
+            $tbox.after('<div id="'+uuid+'" class="crab-view"></div>');
+            return $('#'+uuid).get(0);
+        })(_self._$tbox));
+        _self.links = [];
+        _self._display = options.display || null;
+        _self._getLinksFromStr=function(str){
+        	return (str || "").match(/(http:|https:)?\/\/\S+/ig) || [];
+        };
+        /**
+         * @param links Array|string 
+         * @param callback function(Array|ExternalContent) Callback function accepting array of ExternalContent
+         */
+        _self._getContentFromLinks=function(links,callback){
+        	var contents = [];
+        	
+        	if(callback && typeof callback == 'function')
+        		callback(contents);
+
+        	return contents;
+        };
+        /**
+         * Append contents to node 
+         * @param contents Array|ExternalContent
+         */
+        _self._appendContentToNode=function(contents){
+             (contents || []).map(function(cn){
+                 _self._$viewNode.append(
+                     cn.display()
+                 );	
+             });
+        }
+
+        /*Event Listeners*/
+        /**
+         * @param evt Event
+         */
+        _self._appendOutputOnTextChange=function(evt){
+            var newLinks = _self._getLinksFromStr(_self._$tbox.html())
+                                .filter(function(link){
+                                            var isNew = _self.links.indexOf(link) > -1;
+                                            if(isNew)
+                                                _self.links.push(link);
+                                            return isNew;
+                                }) || [];
+            _self._getContentFromLinks(newLinks,_self._appendContentToNode)
+        };
  	}
 
     if(w instanceof Object){
